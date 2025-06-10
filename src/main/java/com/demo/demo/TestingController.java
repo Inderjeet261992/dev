2e2a3369798f5d92d1b736cc36b6c4c39d32a2a1
Inderjeet261992 <inderjeetkaurhorahora@gmail.com>
2025-06-10 13:57:24 +0530

    Update TestingController.java

--- src/main/java/com/demo/demo/TestingController.java
+++ src/main/java/com/demo/demo/TestingController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class TestingController {
 
-	    @GetMapping("hello")
+	    @GetMapping("/hello")
 	    public String sayHello() {
 	        return "Hello, World!";
 	    }
