68785dc3326bb6f9c3566d810b6f6b3c9f512cd7
Inderjeet261992 <inderjeetkaurhorahora@gmail.com>
2025-06-13 09:53:19 +0530

    Update DemoApplication.java

--- src/main/java/com/demo/demo/DemoApplication.java
+++ src/main/java/com/demo/demo/DemoApplication.java
@@ -1,22 +1,15 @@
 package com.demo.demo;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class DemoApplication  extends SpringBootServletInitializer{
-	
-	 @Override
-	    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	        return builder.sources(DemoApplication.class);
-	    }
-
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
-
